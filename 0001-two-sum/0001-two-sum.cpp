d6459367aa57e3f5d7c70a8faa6b0ccf42e39bb3
Aditya Prakash <aadityaprakash258@gmail.com>
2026-06-02 23:01:03 +0530

    0001-TWO-SUM Stats: Time: 132 ms (5.09%), Space: 14.1 MB (68.81%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      int n=nums.size();
+      int i,j;
+      int sum;
+      for(i=0;i<n-1;i++)
+      {
+        for(j=0;j<n;j++)
+        {
+            if(i==j)
+            {
+                continue;
+            }
+            else
+            {
+                sum=nums[i]+nums[j];
+                        if(sum==target)
+        break;
+            }
+        }
+        if(sum==target)
+        break;
+      }  
+      return{i,j};
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Leetcode
 This is just to track my leetcode progress
+
+<!---LeetCode Topics Start-->
+# LeetCode Topics
+## Array
+|  |
+| ------- |
+| [0001-two-sum](https://github.com/Just-Adii/Leetcode/tree/master/0001-two-sum) |
+## Hash Table
+|  |
+| ------- |
+| [0001-two-sum](https://github.com/Just-Adii/Leetcode/tree/master/0001-two-sum) |
+<!---LeetCode Topics End-->
